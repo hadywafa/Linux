74582f0eea3ba800650177f411c80f6c69b13936
hadywafa <97hadywafa@gmail.com>
2024-07-20 19:55:22 +0300

    update

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,63 +1,65 @@
 #!/bin/bash
 
-# Function to install necessary packages
-install_packages() {
-    if [ -f /etc/debian_version ]; then
-        echo "Detected Debian-based system"
-        sudo apt update
-        sudo apt install -y zsh curl git net-tools iputils-ping
-    elif [ -f /etc/fedora-release ]; then
-        echo "Detected Fedora-based system"
-        sudo dnf update -y
-        sudo dnf install -y zsh curl git net-tools iputils
-    else
-        echo "Unsupported OS"
-        exit 1
-    fi
+# Function to install necessary packages for Debian-based systems
+install_debian_packages() {
+    echo "Detected Debian-based system"
+    sudo apt update
+    sudo apt install -y zsh curl git net-tools iputils-ping
 }
 
-# Function to install Zsh and Oh My Zsh for a specified user
-install_zsh() {
-    local user=$1
+# Function to install necessary packages for Fedora-based systems
+install_fedora_packages() {
+    echo "Detected Fedora-based system"
+    sudo dnf update -y
+    sudo dnf install -y zsh curl git net-tools iputils
+}
 
+# Function to install Zsh and Oh My Zsh
+install_zsh() {
     # Install Oh My Zsh
-    sudo -u $user sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     # Set the default shell to Zsh
-    sudo chsh -s $(which zsh) $user
+    chsh -s $(which zsh)
 
     # Set Zsh theme to agnoster
-    sudo -u $user sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' /home/$user/.zshrc
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
 }
 
-# Function to install plugins for a specified user
+# Function to install plugins
 install_plugins() {
-    local user=$1
-    local zsh_custom="/home/$user/.oh-my-zsh/custom"
+    ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
     # Install zsh-autosuggestions
-    sudo -u $user git clone https://github.com/zsh-users/zsh-autosuggestions $zsh_custom/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
     # Install zsh-syntax-highlighting
-    sudo -u $user git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
     # Install z
-    sudo -u $user git clone https://github.com/rupa/z.git $zsh_custom/plugins/z
+    git clone https://github.com/rupa/z.git $ZSH_CUSTOM/plugins/z
 
     # Add plugins to .zshrc
-    sudo -u $user sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting z)/' /home/$user/.zshrc
+    sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting z)/' ~/.zshrc
 
     # Apply changes
-    sudo -u $user zsh -c "source /home/$user/.zshrc"
+    source ~/.zshrc
 }
 
 # Main function
 main() {
-    local user="ansible"
+    # Detect OS and install necessary packages
+    if [ -f /etc/debian_version ]; then
+        install_debian_packages
+    elif [ -f /etc/fedora-release ]; then
+        install_fedora_packages
+    else
+        echo "Unsupported OS"
+        exit 1
+    fi
 
-    install_packages
-    install_zsh $user
-    install_plugins $user
+    install_zsh
+    install_plugins
     echo "Zsh setup complete. Please restart your terminal."
 }
 
